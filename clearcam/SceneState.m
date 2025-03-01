e2b6f6cf422a48e9f7ac9c75ed5bed68d90c1a72
roryclear <roryclear.rc@gmail.com>
2025-03-01 11:20:36 +0000

    mostly broken downloads page

--- clearcam/SceneState.m
+++ clearcam/SceneState.m
@@ -21,36 +21,47 @@
 
 - (void)processOutput:(NSArray *)array {
     NSMutableDictionary *frame = [[NSMutableDictionary alloc] init];
-    for(int i = 0; i < array.count; i++){
+    
+    // Count occurrences in the current frame
+    for (int i = 0; i < array.count; i++) {
         frame[array[i][4]] = frame[array[i][4]] ? @([frame[array[i][4]] intValue] + 1) : @1;
     }
-    [self.lastN addObject:frame];
     
-    for(int i = 0; i < self.events.count; i++){
+    [self.lastN addObject:frame]; // Store this frame's data
+
+    // Process events
+    for (int i = 0; i < self.events.count; i++) {
         NSNumber *totalValue = self.lastN_total[self.events[i]] ?: @0;
         NSNumber *last_totalValue = [totalValue copy];
         NSNumber *frameValue = frame[self.events[i]] ?: @0;
         totalValue = @(totalValue.intValue + frameValue.intValue);
         
-        if(self.lastN.count > 10){
+        if (self.lastN.count > 10) {
             frameValue = self.lastN[0][self.events[i]] ?: @0;
             totalValue = @(totalValue.intValue - frameValue.intValue);
         }
+        
         int current_state = (int)roundf((totalValue ? [totalValue floatValue] : 0.0) / 10.0);
         int last_state = (int)roundf((last_totalValue ? [last_totalValue floatValue] : 0.0) / 10.0);
         self.lastN_total[self.events[i]] = totalValue;
+        
         if (current_state != last_state) {
+            // Get current timestamp
+            NSDate *date = [NSDate date]; // NOW inside the loop (fix)
+            NSTimeInterval unixTimestamp = [date timeIntervalSince1970]; // Float UNIX timestamp
+            
+            // Format human-readable timestamp for logging
+            NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+            [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+            NSString *formattedTimestamp = [dateFormatter stringFromDate:date];
+
+            // Logging
             NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
             NSString *documentsDirectory = [paths firstObject];
 
             NSFileManager *fileManager = [NSFileManager defaultManager];
-
-            NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-            NSDate *date = [NSDate date];
-            [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
-            NSString *timestamp = [dateFormatter stringFromDate:date];
-
-            NSString *contentToWrite = [NSString stringWithFormat:@"%@ class: %@ x%@\n", timestamp, self.events[i], @(current_state)];
+            NSString *contentToWrite = [NSString stringWithFormat:@"%@ class: %@ x%@\n",
+                                        formattedTimestamp, self.events[i], @(current_state)];
 
             // Handle alerts.txt if needed
             if (current_state == [self.alerts[self.events[i]] intValue]) {
@@ -69,7 +80,7 @@
                 NSManagedObject *newEvent = [NSEntityDescription insertNewObjectForEntityForName:@"EventEntity"
                                                                           inManagedObjectContext:self.backgroundContext];
 
-                [newEvent setValue:@([date timeIntervalSince1970]) forKey:@"timeStamp"]; // Float value of timestamp
+                [newEvent setValue:@(unixTimestamp) forKey:@"timeStamp"]; // Correct per-event timestamp
                 [newEvent setValue:self.events[i] forKey:@"classType"]; // Class type
                 [newEvent setValue:@(current_state) forKey:@"quantity"]; // Quantity (current state)
 
@@ -80,9 +91,12 @@
             }];
         }
     }
-    if(self.lastN.count > 10){
+    
+    // Keep lastN size within 10
+    if (self.lastN.count > 10) {
         [self.lastN removeObjectAtIndex:0];
     }
 }
+
 @end
 
