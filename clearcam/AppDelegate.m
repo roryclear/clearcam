dcc0e4d49ad66cd74e8d2bd81d3fd067d1c8be20
roryclear <roryclear.rc@gmail.com>
2025-03-31 23:23:58 +0100

    start notifications

--- clearcam/AppDelegate.m
+++ clearcam/AppDelegate.m
@@ -1,11 +1,86 @@
 #import "AppDelegate.h"
+#import <UserNotifications/UserNotifications.h>
 
-@interface AppDelegate ()
+@interface AppDelegate () <UNUserNotificationCenterDelegate>
+
 @end
 
 @implementation AppDelegate
 
 @synthesize persistentContainer = _persistentContainer;
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    center.delegate = self; // Set the delegate
+
+    [center requestAuthorizationWithOptions:(UNAuthorizationOptionAlert | UNAuthorizationOptionSound | UNAuthorizationOptionBadge)
+                          completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        if (granted) {
+            NSLog(@"Notification permission granted.");
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [[UIApplication sharedApplication] registerForRemoteNotifications];
+            });
+        } else {
+            NSLog(@"Notification permission denied: %@", error);
+        }
+    }];
+
+    return YES;
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    NSString *token = [self stringFromDeviceToken:deviceToken];
+    NSLog(@"Device Token: %@", token);
+    // Send this token to your backend server.
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    NSLog(@"Failed to register for remote notifications: %@", error);
+}
+
+- (NSString *)stringFromDeviceToken:(NSData *)deviceToken {
+    const char *data = [deviceToken bytes];
+    NSMutableString *token = [NSMutableString string];
+    for (NSUInteger i = 0; i < [deviceToken length]; i++) {
+        [token appendFormat:@"%02.2hhx", data[i]];
+    }
+    return [token copy];
+}
+
+// Handle incoming notifications while app is in the foreground
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+       willPresentNotification:(UNNotification *)notification
+         withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+    completionHandler(UNNotificationPresentationOptionAlert | UNNotificationPresentationOptionSound | UNNotificationPresentationOptionBadge);
+}
+
+//Handle user interaction with notifications (tap)
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+didReceiveNotificationResponse:(UNNotificationResponse *)response
+         withCompletionHandler:(void (^)(void))completionHandler {
+    // Handle notification tap here.
+    completionHandler();
+}
+
+// Handle silent notifications and notifications while app is in background/closed.
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    NSLog(@"Received remote notification: %@", userInfo);
+
+    if (userInfo[@"aps"][@"content-available"]) {
+        // Silent notification handling
+        NSLog(@"Received silent notification.");
+
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            // Perform background tasks here.
+            sleep(5); // Simulate a background task. Replace with actual task.
+            completionHandler(UIBackgroundFetchResultNewData); // Or UIBackgroundFetchResultNoData
+            NSLog(@"Silent notification background task completed.");
+        });
+    } else {
+        // Regular notification handling.
+        completionHandler(UIBackgroundFetchResultNewData);
+    }
+}
 
 - (NSPersistentContainer *)persistentContainer {
     if (_persistentContainer == nil) {
@@ -19,4 +94,5 @@
     }
     return _persistentContainer;
 }
+
 @end
