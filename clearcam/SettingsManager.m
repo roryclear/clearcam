dbcc6abc7e2c227c101e87e72a4d8857cda5224f
roryclear <roryclear.rc@gmail.com>
2025-06-16 15:57:31 +0100

    add default preset names to strings files

--- clearcam/SettingsManager.m
+++ clearcam/SettingsManager.m
@@ -17,11 +17,11 @@
         NSDictionary *savedPresets = [[NSUserDefaults standardUserDefaults] objectForKey:@"yolo_presets"];
         if (!savedPresets) {
             [[NSUserDefaults standardUserDefaults] setObject:[[NSMutableDictionary alloc] initWithDictionary:@{
-                @"all": @[@0, @1, @2, @3, @4, @5, @6, @7, @8, @9, @10, @11, @12, @13, @14, @15, @16, @17, @18, @19, @20, @21, @22, @23, @24, @25, @26, @27, @28, @29, @30, @31, @32, @33, @34, @35, @36, @37, @38, @39, @40, @41, @42, @43, @44, @45, @46, @47, @48, @49, @50, @51, @52, @53, @54, @55, @56, @57, @58, @59, @60, @61, @62, @63, @64, @65, @66, @67, @68, @69, @70, @71, @72, @73, @74, @75, @76, @77, @78, @79],
-                @"People+Vehicles": @[@0, @1, @2, @3, @5, @7]
+                NSLocalizedString(@"all", nil): @[@0, @1, @2, @3, @4, @5, @6, @7, @8, @9, @10, @11, @12, @13, @14, @15, @16, @17, @18, @19, @20, @21, @22, @23, @24, @25, @26, @27, @28, @29, @30, @31, @32, @33, @34, @35, @36, @37, @38, @39, @40, @41, @42, @43, @44, @45, @46, @47, @48, @49, @50, @51, @52, @53, @54, @55, @56, @57, @58, @59, @60, @61, @62, @63, @64, @65, @66, @67, @68, @69, @70, @71, @72, @73, @74, @75, @76, @77, @78, @79],
+                NSLocalizedString(@"people+vehicles", nil): @[@0, @1, @2, @3, @5, @7]
             }] forKey:@"yolo_presets"];
         }
-        if(![[NSUserDefaults standardUserDefaults] objectForKey:@"yolo_preset_idx"]) [[NSUserDefaults standardUserDefaults] setObject:@"People+Vehicles" forKey:@"yolo_preset_idx"];
+        if(![[NSUserDefaults standardUserDefaults] objectForKey:@"yolo_preset_idx"]) [[NSUserDefaults standardUserDefaults] setObject:NSLocalizedString(@"people+vehicles", nil) forKey:@"yolo_preset_idx"];
         [[NSUserDefaults standardUserDefaults] synchronize];
         [self loadResolutionSettings];
     }
