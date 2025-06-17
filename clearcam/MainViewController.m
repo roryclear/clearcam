21649a0937e699170d5c8f10aeaba13ba41ea85d
roryclear <roryclear.rc@gmail.com>
2025-06-17 11:52:51 +0100

    fix wifi label

--- clearcam/MainViewController.m
+++ clearcam/MainViewController.m
@@ -8,6 +8,9 @@
 @interface MainViewController ()
 @property (nonatomic, strong) FileServer *fileServer;
 @property (nonatomic, strong) UILabel *ipLabel;
+@property (nonatomic, strong) UIStackView *mainStackView;
+@property (nonatomic, strong) NSLayoutConstraint *mainStackViewTopToIPLabelConstraint;
+@property (nonatomic, strong) NSLayoutConstraint *mainStackViewTopToSafeAreaConstraint;
 @end
 
 @implementation MainViewController
@@ -24,45 +27,59 @@
 
     self.title = @"Clearcam";
     self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"home", nil)
-                                                                             style:UIBarButtonItemStylePlain
-                                                                            target:nil
-                                                                            action:nil];
+                                                                            style:UIBarButtonItemStylePlain
+                                                                           target:nil
+                                                                           action:nil];
 
     // Setup IP label
-    [self updateIPAddressLabel];
-
-    NSLayoutYAxisAnchor *stackTopAnchor = self.ipLabel ? self.ipLabel.bottomAnchor : self.view.safeAreaLayoutGuide.topAnchor;
-
-    // Stack View setup
-    UIStackView *stackView = [[UIStackView alloc] init];
-    stackView.axis = UILayoutConstraintAxisVertical;
-    stackView.distribution = UIStackViewDistributionFillEqually;
-    stackView.alignment = UIStackViewAlignmentFill;
-    stackView.spacing = 20;
-    stackView.translatesAutoresizingMaskIntoConstraints = NO;
-    [self.view addSubview:stackView];
+    self.ipLabel = [[UILabel alloc] init];
+    self.ipLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    self.ipLabel.font = [UIFont systemFontOfSize:14 weight:UIFontWeightMedium];
+    self.ipLabel.textColor = [UIColor secondaryLabelColor];
+    self.ipLabel.textAlignment = NSTextAlignmentCenter;
+    self.ipLabel.numberOfLines = 1;
+    [self.view addSubview:self.ipLabel];
 
     [NSLayoutConstraint activateConstraints:@[
-        [stackView.topAnchor constraintEqualToAnchor:stackTopAnchor constant:20],
-        [stackView.leadingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leadingAnchor constant:20],
-        [stackView.trailingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.trailingAnchor constant:-20],
-        [stackView.bottomAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.bottomAnchor constant:-20]
+        [self.ipLabel.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:10],
+        [self.ipLabel.leadingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leadingAnchor constant:20],
+        [self.ipLabel.trailingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.trailingAnchor constant:-20]
     ]];
 
-    [self addItemToStackView:stackView
-                       title:NSLocalizedString(@"camera_desc", nil)
-                       image:[UIImage systemImageNamed:@"camera.fill"]
-                      action:@selector(cameraTapped)];
-    
-    [self addItemToStackView:stackView
-                       title:NSLocalizedString(@"gallery_desc", nil)
-                       image:[UIImage systemImageNamed:@"photo.on.rectangle"]
-                      action:@selector(galleryTapped)];
-    
-    [self addItemToStackView:stackView
-                       title:NSLocalizedString(@"settings_desc", nil)
-                       image:[UIImage systemImageNamed:@"gear"]
-                      action:@selector(settingsTapped)];
+    // Stack View setup
+    self.mainStackView = [[UIStackView alloc] init];
+    self.mainStackView.axis = UILayoutConstraintAxisVertical;
+    self.mainStackView.distribution = UIStackViewDistributionFillEqually;
+    self.mainStackView.alignment = UIStackViewAlignmentFill;
+    self.mainStackView.spacing = 20;
+    self.mainStackView.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addSubview:self.mainStackView];
+
+    // Create the two possible top constraints for the stack view
+    // The constant for mainStackViewTopToIPLabelConstraint should ensure there's enough space below the label
+    self.mainStackViewTopToIPLabelConstraint = [self.mainStackView.topAnchor constraintEqualToAnchor:self.ipLabel.bottomAnchor constant:20]; // Keep original constant
+    self.mainStackViewTopToSafeAreaConstraint = [self.mainStackView.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:20]; // Keep original constant
+
+    [NSLayoutConstraint activateConstraints:@[
+        [self.mainStackView.leadingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leadingAnchor constant:20],
+        [self.mainStackView.trailingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.trailingAnchor constant:-20],
+        [self.mainStackView.bottomAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.bottomAnchor constant:-20]
+    ]];
+
+    [self addItemToStackView:self.mainStackView
+                     title:NSLocalizedString(@"camera_desc", nil)
+                     image:[UIImage systemImageNamed:@"camera.fill"]
+                    action:@selector(cameraTapped)];
+
+    [self addItemToStackView:self.mainStackView
+                     title:NSLocalizedString(@"gallery_desc", nil)
+                     image:[UIImage systemImageNamed:@"photo.on.rectangle"]
+                    action:@selector(galleryTapped)];
+
+    [self addItemToStackView:self.mainStackView
+                     title:NSLocalizedString(@"settings_desc", nil)
+                     image:[UIImage systemImageNamed:@"gear"]
+                    action:@selector(settingsTapped)];
 
     // Observe IP address changes
     [[NSNotificationCenter defaultCenter] addObserver:self
@@ -71,31 +88,35 @@
                                                object:nil];
 }
 
-- (void)updateIPAddressLabel {
-    NSString *ipAddress = [[NSUserDefaults standardUserDefaults] stringForKey:@"DeviceIPAddress"];
-    if (ipAddress) {
-        if (!self.ipLabel) {
-            self.ipLabel = [[UILabel alloc] init];
-            self.ipLabel.translatesAutoresizingMaskIntoConstraints = NO;
-            self.ipLabel.font = [UIFont systemFontOfSize:14 weight:UIFontWeightMedium];
-            self.ipLabel.textColor = [UIColor secondaryLabelColor];
-            self.ipLabel.textAlignment = NSTextAlignmentCenter;
-            self.ipLabel.numberOfLines = 1;
-            [self.view addSubview:self.ipLabel];
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self updateIPAddressLabel]; // Ensure layout is correct when view appears/reappears
+}
 
-            [NSLayoutConstraint activateConstraints:@[
-                [self.ipLabel.topAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor constant:10],
-                [self.ipLabel.leadingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leadingAnchor constant:20],
-                [self.ipLabel.trailingAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.trailingAnchor constant:-20]
-            ]];
-        }
-        self.ipLabel.text = (ipAddress.length > 0) ? [NSString stringWithFormat:@"Streaming over local Wi-Fi at: %@", ipAddress] : @"Waiting for Wi-Fi...";
+- (void)updateIPAddressLabel {
+    BOOL streamViaWifiEnabled = [[NSUserDefaults standardUserDefaults] boolForKey:@"stream_via_wifi_enabled"];
+    NSString *ipAddress = [[NSUserDefaults standardUserDefaults] stringForKey:@"DeviceIPAddress"];
+
+    if (streamViaWifiEnabled) {
+        // If streaming is enabled, IP label should be visible
         self.ipLabel.hidden = NO;
+        self.ipLabel.text = (ipAddress.length > 0) ? [NSString stringWithFormat:@"Streaming over local Wi-Fi at: %@", ipAddress] : @"Waiting for Wi-Fi...";
+
+        // Activate constraint to IP label, deactivate constraint to safe area
+        self.mainStackViewTopToSafeAreaConstraint.active = NO;
+        self.mainStackViewTopToIPLabelConstraint.active = YES;
     } else {
-        if (self.ipLabel) {
-            self.ipLabel.hidden = YES;
-        }
+        // If streaming is disabled, IP label should be hidden
+        self.ipLabel.hidden = YES;
+        self.ipLabel.text = nil; // Clear text when hidden
+
+        // Activate constraint to safe area, deactivate constraint to IP label
+        self.mainStackViewTopToIPLabelConstraint.active = NO;
+        self.mainStackViewTopToSafeAreaConstraint.active = YES;
     }
+
+    // Force layout update after constraint changes
+    [self.view layoutIfNeeded];
 }
 
 - (void)addItemToStackView:(UIStackView *)stackView title:(NSString *)title image:(UIImage *)image action:(SEL)action {
@@ -169,4 +190,3 @@
 }
 
 @end
-
