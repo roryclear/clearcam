503217c84c18b15f3f178a06f829b7ff3606e07b
roryclear <roryclear.rc@gmail.com>
2025-10-05 22:50:32 +0800

    tg resize

--- clearcam.py
+++ clearcam.py
@@ -30,6 +30,13 @@ import platform
 import ctypes
 import zlib
 
+def resize(img, new_size):
+    img = Tensor(img)
+    img = img.permute(2,0,1)
+    img = Tensor.interpolate(img, size=(new_size[1], new_size[0]), mode='linear', align_corners=False)
+    img = img.permute(1, 2, 0)
+    return img
+
 def preprocess(image, new_shape=1280, auto=True, scaleFill=False, scaleup=True, stride=32) -> Tensor:
   shape = image.shape[:2]  # current shape [height, width]
   new_shape = (new_shape, new_shape) if isinstance(new_shape, int) else new_shape
@@ -41,8 +48,7 @@ def preprocess(image, new_shape=1280, auto=True, scaleFill=False, scaleup=True, 
   new_unpad = (new_shape[1], new_shape[0]) if scaleFill else new_unpad
   dw /= 2
   dh /= 2
-  image = Tensor(image)
-  image = resize(image, new_unpad) if shape[::-1] != new_unpad else image # todo resize is np
+  image = resize(image, new_unpad)
   top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
   left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
   image = copy_make_border(image, top, bottom, left, right, value=(114,114,114))
@@ -78,42 +84,6 @@ def make_anchors(feats, strides, grid_cell_offset=0.5):
 
 def copy_make_border(img, top, bottom, left, right, value=(0, 0, 0)):
     return img.pad(((top,top),(left,left),(0,0)))
-
-
-def resize(img, new_size):
-    img = img.numpy()
-    h, w = img.shape[:2]
-    new_w, new_h = new_size
-    if (w, h) == (new_w, new_h): return Tensor(img)
-
-    y = np.linspace(0, h - 1, new_h)
-    x = np.linspace(0, w - 1, new_w)
-    xv, yv = np.meshgrid(x, y)
-
-    x0 = np.floor(xv).astype(int)
-    x1 = np.clip(x0 + 1, 0, w - 1)
-    y0 = np.floor(yv).astype(int)
-    y1 = np.clip(y0 + 1, 0, h - 1)
-
-    wx = xv - x0
-    wy = yv - y0
-
-    Ia = img[y0, x0]
-    Ib = img[y1, x0]
-    Ic = img[y0, x1]
-    Id = img[y1, x1]
-
-    wa = (1 - wx) * (1 - wy)
-    wb = (1 - wx) * wy
-    wc = wx * (1 - wy)
-    wd = wx * wy
-
-    out = (Ia * wa[..., None] +
-           Ib * wb[..., None] +
-           Ic * wc[..., None] +
-           Id * wd[..., None])
-
-    return Tensor(out.astype(img.dtype))
 
 # this function is from the original implementation
 def autopad(k, p=None, d=1):  # kernel, padding, dilation
