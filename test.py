92ec61072a82c36fce376fd140b738f3f658f3cd
Rory Clear <45880510+roryclear@users.noreply.github.com>
2025-11-20 15:20:12 +0000

    Linear sum assignment (#99)
    * own linear_sum_assignment
    
    * start a test file
    
    * kwok algo
    
    * print time
    
    * clean
    
    * use other func
    
    * keep using scipy for now
    
    * linear_sum_assignment2
    
    * donprint

---
+++ test.py
@@ -0,0 +1,32 @@
+import numpy as np
+from scipy.optimize import linear_sum_assignment as scipy_linear_sum_assignment
+from yolox.tracker.matching import linear_sum_assignment2
+import time
+
+def test_linear_sum_assigment():
+  for i in range(1,300):
+    input = np.random.rand(i,i)
+    a, b = linear_sum_assignment2(input)
+    c, d = scipy_linear_sum_assignment(input)
+    np.testing.assert_allclose(a,c)
+    np.testing.assert_allclose(b,d)
+    
+def test_linear_sum_assigment_speed():
+  total_scipy = 0
+  total = 0
+  for i in range(1,300):
+    input = np.random.rand(i,i)
+    st = time.perf_counter()
+    linear_sum_assignment2(input)
+    t = time.perf_counter() - st
+    total += t
+    st = time.perf_counter()
+    scipy_linear_sum_assignment(input)
+    t_scipy = time.perf_counter() - st
+    total_scipy += t_scipy
+    print(t / t_scipy, i)
+    #np.testing.assert_(t < t_scipy*50, f"test_linear_sum_assigment_speed slow from {i}")
+  print("time vs scipy =", f"{(total / total_scipy) * 100:.1f}%")
+
+test_linear_sum_assigment()
+test_linear_sum_assigment_speed()
