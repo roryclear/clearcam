a9c02580901266c90e62fc75420ca26ed5872db7
roryclear <roryclear.rc@gmail.com>
2025-11-21 21:10:45 +0000

    no need to remove dups

--- yolox/tracker/byte_tracker2.py
+++ yolox/tracker/byte_tracker2.py
@@ -301,7 +301,7 @@ class BYTETracker2(object):
         self.lost_stracks.extend(lost_stracks)
         self.lost_stracks = sub_stracks(self.lost_stracks, self.removed_stracks)
         self.removed_stracks.extend(removed_stracks)
-        self.tracked_stracks, self.lost_stracks = remove_duplicate_stracks(self.tracked_stracks, self.lost_stracks)
+
         # get scores of lost tracks
         output_stracks = [track for track in self.tracked_stracks if track.is_activated]
 
