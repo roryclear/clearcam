5d740450fa7b7a28bd59ebd554bad5cd06204538
roryclear <roryclear.rc@gmail.com>
2026-03-10 12:18:52 +0000

    start detr test

---
+++ test/test_mot2.py
@@ -0,0 +1,121 @@
+from detection.rfdetr import LWDETR
+import cv2
+from tinygrad import Tensor, TinyJit
+from tinygrad.dtype import dtypes
+import numpy as np
+from pathlib import Path
+
+COCO_CLASSES = {1: "person", 2: "bicycle", 3: "car", 4: "motorcycle", 5: "airplane", 6: "bus", 7: "train", 8: "truck", 9: "boat",
+10: "traffic light", 11: "fire hydrant", 13: "stop sign", 14: "parking meter", 15: "bench", 16: "bird", 17: "cat", 18: "dog",
+19: "horse", 20: "sheep", 21: "cow", 22: "elephant", 23: "bear", 24: "zebra", 25: "giraffe", 27: "backpack", 28: "umbrella",
+31: "handbag", 32: "tie", 33: "suitcase", 34: "frisbee", 35: "skis", 36: "snowboard", 37: "sports ball", 38: "kite", 39: "baseball bat",
+40: "baseball glove", 41: "skateboard", 42: "surfboard", 43: "tennis racket", 44: "bottle", 46: "wine glass", 47: "cup", 48: "fork",
+49: "knife", 50: "spoon", 51: "bowl", 52: "banana", 53: "apple", 54: "sandwich", 55: "orange", 56: "broccoli", 57: "carrot",
+58: "hot dog", 59: "pizza", 60: "donut", 61: "cake", 62: "chair", 63: "couch", 64: "potted plant", 65: "bed", 67: "dining table",
+70: "toilet", 72: "tv", 73: "laptop", 74: "mouse", 75: "remote", 76: "keyboard", 77: "cell phone", 78: "microwave", 79: "oven",
+80: "toaster", 81: "sink", 82: "refrigerator", 84: "book", 85: "clock", 86: "vase", 87: "scissors", 88: "teddy bear",
+89: "hair drier", 90: "toothbrush",
+}
+
+@TinyJit
+def do_inf(im, model, h, w):
+  return model.predict(im, h, w)
+
+def letterbox(im, new_shape=(960, 960), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
+    shape = im.shape[:2]
+    if isinstance(new_shape, int): new_shape = (new_shape, new_shape)
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    if not scaleup: r = min(r, 1.0)
+
+    ratio = r, r
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]
+    if auto:
+        dw, dh = np.mod(dw, stride), np.mod(dh, stride)
+    elif scaleFill:  # stretch
+        dw, dh = 0.0, 0.0
+        new_unpad = (new_shape[1], new_shape[0])
+        ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]
+
+    dw /= 2
+    dh /= 2
+
+    if shape[::-1] != new_unpad: im = cv2.resize(im, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    im = cv2.copyMakeBorder(im, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
+    return im, ratio, (dw, dh)
+
+def scale_coords(boxes, ratio, dwdh):
+  boxes[:, [0, 2]] -= dwdh[0]
+  boxes[:, [1, 3]] -= dwdh[1]
+  boxes[:, :4] /= ratio[0]
+  return boxes
+
+def resize(img, new_size):
+  img = img.permute(2,0,1)
+  img = Tensor.interpolate(img, size=(new_size[1], new_size[0]), mode='linear', align_corners=False)
+  img = img.permute(1, 2, 0)
+  return img
+
+def preprocess(img, res):
+  print("rory pre pre shape =",img.shape)
+  means = Tensor([[[0.485, 0.456, 0.406]]])
+  stds = Tensor([[[0.229, 0.224, 0.225]]])
+  img = resize(img, (res, res))
+  img = (img - means) / stds
+  img = img.permute(2, 0, 1).unsqueeze(0)
+  return img
+
+if __name__ == "__main__":
+  from ocsort_tracker import ocsort
+  ocs_tracker = ocsort.OCSort(max_age=60)
+
+  size = "t"
+  #model = YOLOv9(*SIZES[size]) if size in SIZES else YOLOv9()
+  model = LWDETR("nano")
+  #state_dict = safe_load(fetch(f'https://huggingface.co/roryclear/yolov9/resolve/main/yolov9-{size}.safetensors'))
+  #load_state_dict(model, state_dict)
+  Path('./test_outputs').mkdir(parents=True, exist_ok=True)
+
+  trackers = [ocs_tracker]
+  excepted_ppl = [162]
+
+  cap = cv2.VideoCapture("test/videos/MOT16-03.mp4")
+  w, h = int(cap.get(3)), int(cap.get(4))
+
+  i = 0
+  ppl = set()
+  threshold = 0.25
+  while True:
+    ret, im0 = cap.read()
+    if not ret: break
+    im = im0
+    im = Tensor(im).cast(dtype=dtypes.float32)
+    h, w = im.shape[:2]
+    im /= 255.0 # IMPORTANT
+    im = preprocess(im, 384)
+    output = do_inf(im, model, h, w).numpy()      
+    
+    boxes = output[:, :4]
+    scores = output[:, 4]
+    class_ids = output[:, 5].astype(int)
+
+    keep = scores > threshold
+    scores = scores[keep]
+    class_ids = class_ids[keep]
+    boxes = boxes[keep]
+
+    labels = [f"{COCO_CLASSES[class_id]}" for class_id in class_ids]
+    annotated_image = im0.copy()
+
+    for box, label, class_id in zip(boxes, labels, class_ids):
+      x1, y1, x2, y2 = map(int, box)
+      color = ((int(class_id)*37)%255, (int(class_id)*17)%255, (int(class_id)*97)%255); cv2.rectangle(annotated_image, (x1, y1), (x2, y2), color, 2); cv2.rectangle(annotated_image, (x1, y1-18), (x1+len(label)*9, y1), color, -1); cv2.putText(annotated_image, label, (x1, y1 - 4), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1, cv2.LINE_AA)
+    cv2.imwrite(f"annotated_image_{i}.jpg", annotated_image)
+    
+
+    
+    i+=1
+    print("frame",i)
+  cap.release()
