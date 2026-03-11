8d97ad50d5b86c684dd870562ea414994b9d053e
roryclear <roryclear.rc@gmail.com>
2026-03-11 12:20:25 +0000

    almost the same

--- test/test_mot.py
+++ test/test_mot.py
@@ -1,5 +1,6 @@
 from detection.yolov9 import YOLOv9, safe_load, load_state_dict, Sequential, Silence, Conv, RepNCSPELAN4, AConv,\
 ADown, CBLinear, CBFuse, SPPELAN, Upsample, Concat, DDetect, postprocess, fetch, draw_bounding_boxes
+from detection.rfdetr import RFDETR, detr_to_yolo
 import cv2
 from tinygrad import Tensor, TinyJit
 from tinygrad.dtype import dtypes
@@ -12,12 +13,14 @@ if __name__ == "__main__":
   ocs_tracker = ocsort.OCSort(max_age=60)
 
   size = "t"
-  model = YOLOv9(size, res=960)
   class_labels = fetch('https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names').read_text().split("\n")
   Path('./test_outputs').mkdir(parents=True, exist_ok=True)
   cap = cv2.VideoCapture("test/videos/MOT16-03.mp4")
   w, h = int(cap.get(3)), int(cap.get(4))
   out = cv2.VideoWriter(f"test_outputs/out_{size}.mp4", cv2.VideoWriter_fourcc(*"mp4v"), 30, (w, h))
+
+  model = YOLOv9(size, res=960)
+  #model = RFDETR("nano", w, h)
 
   i = 0
   ppl = set()
@@ -29,6 +32,8 @@ if __name__ == "__main__":
     im = model.preprocess(im)
     pred = model(im).numpy()
     online_targets = ocs_tracker.update(pred, 0.25)
+    if type(model) == RFDETR: # RF-DETR has different class_ids
+      for j in range(len(online_targets)): online_targets[j].class_id = detr_to_yolo[int(online_targets[j].class_id)]
     preds = []
     for x in online_targets:
       if x.tracklet_len < 1 or x.speed < 2.5: continue
