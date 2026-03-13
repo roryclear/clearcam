d6bfa8d911c0870ff233efbe3aac0c8d59d9c746
roryclear <roryclear.rc@gmail.com>
2026-03-13 10:49:32 +0000

    print time taken

--- test/test_ocsort.py
+++ test/test_ocsort.py
@@ -1,9 +1,11 @@
 import pickle
 from ocsort_tracker import ocsort
 import numpy as np
+import time
 ocs_tracker = ocsort.OCSort(max_age=60)
 tracks = pickle.load(open('test/tracks.pkl', 'rb'))
 
+st = time.time()
 for i in range(len(tracks)):
     out = ocs_tracker.update(tracks[i][0], 0.25)
     preds = []
@@ -11,3 +13,4 @@ for i in range(len(tracks)):
     for x in out: preds.append(np.array([x.tlwh[0], x.tlwh[1], x.tlwh[0] + x.tlwh[2], x.tlwh[1] + x.tlwh[3], x.score, x.class_id]))
     for x in tracks[i][1]: expected.append(np.array([x.tlwh[0], x.tlwh[1], x.tlwh[0] + x.tlwh[2], x.tlwh[1] + x.tlwh[3], x.score, x.class_id]))
     np.testing.assert_allclose(preds, expected, rtol=1e-5)
+print(f"passed in {(time.time() - st):.2f} seconds")
