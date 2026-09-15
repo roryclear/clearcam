52928b7fb843a6bca0602c2908cac8282701a1db
Rory Clear <roryclear.rc@gmail.com>
2026-09-15 19:08:10 +0100

    regression test for newer tg

--- test/test_yolo.py
+++ test/test_yolo.py
@@ -5,17 +5,17 @@ from utils.helpers import jit_infer
 import numpy as np
 if __name__ == "__main__":
   jit_cache = {}
-  img = cv2.imread("test/clip_images/f40.jpg")
+  img = cv2.imread("test/clip_images/front.png")
   img = Tensor(img)
   model = YOLOv9("t", 960)
   for _ in range(3): preds = jit_infer(model, img, jit_cache).numpy()
   ret = []
   print(preds)
   for p in preds:
-    if p[-2] > 0.5: ret.append(list(p))
-  print(ret)
+    if p[-2] > 0.7: ret.append(list(p))
+  print("\n",ret)
   assert len(ret) == 1
-  expected = [70.17624, 212.71173, 895.62067, 517.2943, 0.6730736, 2.0]
+  expected = [511.69214, 367.81128, 1294.1671, 899.5974, 0.8302731, 2.0]
   np.testing.assert_allclose(expected,ret[0], rtol=1e-4)
 
   # sanity test, BEAM is flakey
