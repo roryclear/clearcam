15893baf058b74a9251ac7b75cbba302eca779c7
Rory Clear <roryclear.rc@gmail.com>
2026-09-21 00:22:19 +0100

    clean

--- test/test_yolo.py
+++ test/test_yolo.py
@@ -4,10 +4,10 @@ import numpy as np
 
 if __name__ == "__main__":
   jit_cache = {}
-  img = Tensor.rand((1, 160, 68, 1))
+  img = Tensor.rand((1, 160, 32, 1))
   conv = nn.Conv2d(160, 64, 1, (1,1), (0,0), (1,1), 1, False)
   ret = conv(img).sum().numpy()
-  expected = -240.04724
+  expected = -8.297832
   np.testing.assert_allclose(ret, expected, rtol=1e-4)
 
   # sanity test, BEAM is flakey
